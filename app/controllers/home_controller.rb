85a516bed07ffe304ca41c49c0308ef67d7ab1fb
spenguinlui <spenguin10@yahoo.com.tw>
2021-01-11 13:51:25 +0800

    加入刪除

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,10 @@ class HomeController < ApplicationController
     @user = current_user
     @users = User.all
   end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
+
+  delete "/user/:id", to: "home#destroy"
   
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   namespace :user do
